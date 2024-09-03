e691a12f7d7a827fd39792211b3f057f8fe1e265
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-09-03 15:27:57 +0300

    hotfix missing   shiny::req(r_connectionHandler$cohortTableHandler) on modules

--- R/mod_analysisSettings_CodeWAS.R
+++ R/mod_analysisSettings_CodeWAS.R
@@ -115,6 +115,7 @@ mod_analysisSettings_codeWAS_server <- function(id, r_connectionHandler) {
     # update selectCohorts_pickerInput with cohort names in r_connectionHandler
     #
     shiny::observe({
+      shiny::req(r_connectionHandler$cohortTableHandler)
       shiny::req(r_connectionHandler$hasChangeCounter)
 
       cohortIdAndNames <- r_connectionHandler$cohortTableHandler$getCohortIdAndNames()
