0304ee457767e0d6a5249fac68d061e86b7e2a87
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-09-30 12:16:32 +0300

    Update fct_GWAS.R

--- R/fct_GWAS.R
+++ R/fct_GWAS.R
@@ -162,10 +162,10 @@ assertAnalysisSettings_GWAS <- function(analysisSettings) {
   c('cohortIdCases', 'cohortIdControls', 'phenotype', 'description', 'analysisType', 'release')  |> checkmate::assertSubset(names(analysisSettings))
   analysisSettings$cohortIdCases |> checkmate::assertNumeric()
   analysisSettings$cohortIdControls |> checkmate::assertNumeric()
-  analysisSettings$phenotype |> checkmate::assertString(min.chars = 1, pattern = "^[A-Z]+$")
+  analysisSettings$phenotype |> checkmate::assertString(min.chars = 1, pattern = "^[A-Z0-9]+$")
   analysisSettings$description |> checkmate::assertString(min.chars = 1)
   analysisSettings$analysisType |> checkmate::assertChoice(choices = c("additive", "recessive", "dominant"))
-  analysisSettings$release |> checkmate::assertChoice(choices = c("Regenie"))
+  analysisSettings$release |> checkmate::assertChoice(choices = c("Regenie12"))
   return(analysisSettings)
 }
 
