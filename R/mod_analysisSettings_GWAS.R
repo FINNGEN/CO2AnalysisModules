7152f5eeeebcb7db3d1c622a7a6d44086dbfb44b
javier-gracia-tabuenca-tuni <javier.graciatabuenca@tuni.fi>
2024-12-05 14:21:50 +0200

    added chmod

--- R/mod_analysisSettings_GWAS.R
+++ R/mod_analysisSettings_GWAS.R
@@ -218,11 +218,13 @@ mod_analysisSettings_GWAS_server <- function(id, r_connectionHandler) {
       if (!file.exists(".sandbox_token")) {
         message <- paste0(message, "\u274C Token not found.\n")
       } else {
+        # try to chmod .sandbox_token
+        Sys.chmod(".sandbox_token", mode = "666")
         token <- readLines(".sandbox_token")
         base_url <- "https://internal-api.app.finngen.fi/internal-api/"
         connectionSandboxAPI <- FinnGenUtilsR::createSandboxAPIConnection(base_url, token)
         if (connectionSandboxAPI$conn_status_tibble$logTibble$type == "ERROR") {
-          message <- paste0(message, "\u274C Error connecting to the sandbox API. Error message: ", connectionSandboxAPI$conn_status$message, "\n")
+          message <- paste0(message, "\u274C Error connecting to the sandbox API. Error message: ", connectionSandboxAPI$conn_status_tibble$logTibble$message, "\n")
           connectionSandboxAPI <- NULL
         } else {
           message <- paste0(message, "\u2705 Token found and connection to the sandbox API successful\n")
