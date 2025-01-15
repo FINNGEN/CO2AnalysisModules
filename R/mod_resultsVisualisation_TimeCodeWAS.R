b4346b87dae870dab72ddfa939bbc37da5d9952e
Harri Siirtola <harri.siirtola@gmail.com>
2025-01-15 09:52:06 +0200

    - checking the input with logger

--- R/mod_resultsVisualisation_TimeCodeWAS.R
+++ R/mod_resultsVisualisation_TimeCodeWAS.R
@@ -171,6 +171,7 @@ mod_resultsVisualisation_TimeCodeWAS_server <- function(id, analysisResults) {
     atlasUrl <- "https://atlas.app.finngen.fi"
 
     studyResults  <- .analysisResultsHandler_to_studyResults(analysisResults)
+    ParallelLogger::log_info("studyResults", studyResults)
 
     last_plot <- NULL
 
