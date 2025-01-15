5d8d8af56e37c4ff36d300a54c58add38f9d2182
Harri Siirtola <harri.siirtola@gmail.com>
2025-01-15 10:08:58 +0200

    Revert "- fix for ParallelLogger call"
    This reverts commit 135aec3501ac39f628f9d4430d16853dd6e70c72.

--- R/mod_resultsVisualisation_TimeCodeWAS.R
+++ R/mod_resultsVisualisation_TimeCodeWAS.R
@@ -171,7 +171,7 @@ mod_resultsVisualisation_TimeCodeWAS_server <- function(id, analysisResults) {
     atlasUrl <- "https://atlas.app.finngen.fi"
 
     studyResults  <- .analysisResultsHandler_to_studyResults(analysisResults)
-    ParallelLogger::logInfo("studyResults", studyResults)
+    ParallelLogger::log_info("studyResults", studyResults)
 
     last_plot <- NULL
 
