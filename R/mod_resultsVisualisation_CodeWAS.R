7346d5976cef08766631246878246d25451ed4b5
Harri Siirtola <harri.siirtola@gmail.com>
2025-02-03 11:45:45 +0200

    - label points that are first in the table

--- R/mod_resultsVisualisation_CodeWAS.R
+++ R/mod_resultsVisualisation_CodeWAS.R
@@ -505,7 +505,7 @@ mod_resultsVisualisation_CodeWAS_server <- function(id, analysisResults) {
           # label the top 10 values
           ggrepel::geom_text_repel(
             data =  df |>
-              dplyr::arrange(desc(pLog10), desc(oddsRatio)) |>
+              dplyr::arrange(desc(beta), desc(pLog10)) |> # this or arrange(desc(pLog10), desc(oddsRatio))?
               dplyr::slice_head(n = input$label_top_n),
             ggplot2::aes(
               label = stringr::str_wrap(stringr::str_trunc(.removeDomain(covariateName), 45), 30)
